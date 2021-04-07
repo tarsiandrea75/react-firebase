e4de6201e3217a8ad2c60fa5805be16292a0e5ba
tarsiandrea75 <tarsiandrea75@gmail.com>
2021-04-07 19:39:49 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import { getFirestore } from "./configs/firebase";
 export default function App() {
   useEffect(() => {
     const db = getFirestore();
-    const categories = db.collection("categories");
+    const categories = db.collection("items");
 
     categories.get().then(res => {
-      console.log(res);
-      console.log(res.size);
+      /*  console.log(res);
+      console.log(res.size); */
       if (res.size > 0) {
+        /*  console.log(res);
+        console.log(res.docs); */
+        res.docs.map(document =>
+          console.log({ id: document.id, ...document.data() })
+        );
       }
     });
   }, []);
--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,7 +1,14 @@
 import firebase from "firebase/app";
-import "firebase/firestaore";
+import "firebase/firestore";
 
-const configFirebase = {};
+const configFirebase = {
+  apiKey: "AIzaSyD3VWQ45QN2U7_kMSdpSp-zxQUQ9yvkfug",
+  authDomain: "camada14520-andrea-tarsi.firebaseapp.com",
+  projectId: "camada14520-andrea-tarsi",
+  storageBucket: "camada14520-andrea-tarsi.appspot.com",
+  messagingSenderId: "361275826888",
+  appId: "1:361275826888:web:c268f93b93b7636391fbfd"
+};
 
 const app = firebase.initializeApp(configFirebase);
 
